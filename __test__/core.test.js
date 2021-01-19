174e0b9c6897cdbf073c017eb2a783cb6372c836
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-01-19 15:22:52 -0300

    configuring jest

---
+++ __test__/core.test.js
@@ -0,0 +1,12 @@
+import createCore from "../src/core.js";
+
+describe('Testing core when imported', () => {
+
+    test('should contain the method #start and #stop', () => {
+
+        // const core = createCore();
+        
+        // expect(core).toHaveProperty('start');
+        // expect(1).to.be(1);
+    });
+});
